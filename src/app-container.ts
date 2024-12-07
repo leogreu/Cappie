0b69932d42c83d077768202df126c06388c4f51b
Leonard Greulich <leonard.greulich@gmail.com>
2024-12-07 16:46:47 +0100

    feat: add background image with blur option

--- src/app-container.ts
+++ src/app-container.ts
@@ -4,6 +4,30 @@ import type { Base64File } from "utils/files.ts";
 // Import all components to be used without import
 import.meta.glob("./**/*.ts", { eager: true });
 
+const Options: {
+    [name: string]: {
+        min: number;
+        max: number;
+        step: number;
+    };
+} = {
+    blur: {
+        min: 0,
+        max: 10,
+        step: 0.5
+    },
+    scale: {
+        min: 0,
+        max: 1,
+        step: 0.01
+    },
+    radius: {
+        min: 0,
+        max: 50,
+        step: 1
+    }
+};
+
 @customElement("app-container")
 export class AppContainer extends AppComponent {
     @state()
@@ -17,26 +41,45 @@ export class AppContainer extends AppComponent {
         }
 
         main {
+            position: relative;
             display: grid;
             place-items: center;
             flex: 2;
             margin: var(--size-12);
-            background-color: var(--surface-2);
-            border-radius: var(--radius-sm);
+            border-radius: var(--radius-md);
+            overflow: hidden;
+        }
+
+        div {
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            background-image: url("public/background-1.jpg");
+            background-size: cover;
+            scale: 1.05;
+
+            filter: blur(calc(var(--blur) * 1px));
         }
 
         img {
+            position: absolute;
             max-width: 100%;
             max-height: 100%;
             object-fit: contain;
             overflow: hidden;
+            box-shadow: var(--shadow-lg) var(--shadow);
 
             scale: var(--scale);
             border-radius: calc(var(--radius) * 1px);
         }
 
+        file-dropzone {
+            position: absolute;
+        }
+
         aside {
             display: flex;
+            flex-direction: column;
             flex: 1;
             justify-content: center;
             align-items: center;
@@ -46,6 +89,7 @@ export class AppContainer extends AppComponent {
     render() {
         return html`
             <main>
+                <div></div>
                 ${this.file
                     ? html`
                         <img src="data:${this.file.mimeType};base64,${this.file.data}">
@@ -58,26 +102,18 @@ export class AppContainer extends AppComponent {
                 }
             </main>
             <aside>
-                Scale
-                <input
-                    name="scale"
-                    type="range"
-                    min="0"
-                    max="1"
-                    step="0.01"
-                    value=${this.getProperty("scale")}
-                    @input=${this.handleNumericInput}
-                >
-                Radius
-                <input
-                    name="radius"
-                    type="range"
-                    min="0"
-                    max="40"
-                    step="1"
-                    value=${this.getProperty("radius")}
-                    @input=${this.handleNumericInput}
-                >
+                ${Object.entries(Options).map(([key, value]) => html`
+                    ${key}
+                    <input
+                        name=${key}
+                        type="range"
+                        min=${value.min}
+                        max=${value.max}
+                        step=${value.step}
+                        value=${this.getProperty(key)}
+                        @input=${this.handleNumericInput}
+                    >
+                `)}
             </aside>
         `;
     }
