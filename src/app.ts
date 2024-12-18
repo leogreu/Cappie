718e3545e14fc10ebdaad32bddba1d07482f1709
Leonard Greulich <leonard.greulich@gmail.com>
2024-12-18 11:51:45 +0100

    refactor: minor code refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import * as router from "utils/router.ts";
 import { ComposedImage } from "./models/composed-image.ts";
 
-const init = async () => {
-    // Import all components to be used without import
-    import.meta.glob("./components/**/*.ts", { eager: true });
+// Import all components to be used without import
+import.meta.glob("./components/**/*.ts", { eager: true });
 
+const init = async () => {
     // For users with existing images, navigate to /home on startup
     if (location.pathname === "/" && await ComposedImage.where().first()) {
         history.replaceState(null, String(), "/home");
